67efb91f2f995a457a4ea750d54cef3fc92624a2
LeonPJ <leon19941214@gmail.com>
2022-09-06 23:14:44 +0800

    add new password and forgor password API

--- src/controller/Auth.ts
+++ src/controller/Auth.ts
@@ -2,21 +2,23 @@ import { Router, Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import cookieSession from 'cookie-session';
-
+import nodemailer from 'nodemailer';
 
 import User from '../models/User';
-import { registerValidation, loginValidation } from '../middleware/validation';
+import { registerValidation, loginValidation, newPasswordValidation, forgotPasswordValidation } from '../middleware/validation';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
     // validate the data
     const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error)
+        return res.status(400).json({ "message": error.details[0].message });
 
     const { name, email, password } = req.body;
 
     // check if user is already in the database
     const emailExit = await User.findOne({ email });
-    if (emailExit) return res.status(400).send('email already exists');
+    if (emailExit)
+        return res.status(400).json({ "message": "email already register" });
 
     // hash password
     const salt = await bcrypt.genSalt(10);
@@ -29,10 +31,9 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
         password: hashedPassword
     });
 
-
     try {
         const saveUser = await user.save();
-        res.status(201).send(saveUser);
+        res.status(200).json({ "message": "register", "status": true, "_id": saveUser._id });
     } catch (error) {
         res.status(400).send(error);
     }
@@ -43,22 +44,112 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 
     // validate the data
     const { error } = loginValidation(req.body);
-    if (error) return res.status(401).send(error.details[0].message);
+    if (error)
+        return res.status(400).json({ "message": error.details[0].message });
 
     // check if email is already in the database
     const user = await User.findOne({ email });
-    if (!user) return res.status(401).end('email is not found');
+    if (!user)
+        return res.status(400).json({ "message": "email is not found" });
 
     // password is correct
     const validPass = await bcrypt.compare(password, user.password);
-    // console.log(user.password);
-    if (!validPass) return res.status(401).end('invalid password');
+    if (!validPass)
+        return res.status(400).json({ "message": "invalid password" });
 
-    // create a token
-    if (user && validPass) {
+    try {
+        // create a token
         const token = jwt.sign({ name: user.name }, process.env.TOKEN_SECRET!, { expiresIn: '24h' });
-        // console.log(token);
-        res.header('auth-token', token).send(token);
-        // res.cookie('auth-token', token);
+        res.status(200).json({ "message": "auth token", "status": true, "token": token });
+    } catch (error) {
+        res.status(400).send(error);
     }
+
+}
+
+export const newPassword = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password, newPassword, confirmNewPassword } = req.body;
+
+    if (newPassword != confirmNewPassword)
+        return res.status(400).json({ "message": "new password not same" });
+
+    // validate the data
+    const { error } = newPasswordValidation(req.body);
+    if (error)
+        return res.status(400).json({ "message": error.details[0].message });
+
+    // check if email is already in the database
+    const user = await User.findOne({ email });
+    if (!user)
+        return res.status(400).json({ "message": "email is not found" })
+
+    // password is correct
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass)
+        return res.status(400).json({ "message": "invalid password" });
+
+    // hash new password
+    const salt = await bcrypt.genSalt(10);
+    const newHashedPassword = await bcrypt.hash(newPassword, salt);
+    const result = await User.updateOne({ email }, { "password": newHashedPassword });
+
+    if (!result.acknowledged || !result.modifiedCount)
+        return res.status(400).json({ "message": "new password changed", "status": false });
+
+    res.status(200).json({ "message": "new password changed", "status": true });
+}
+
+export const forgotPassword = async (req: Request, res: Response, next: NextFunction) => {
+    const { email } = req.body;
+
+    // validate the data
+    const { error } = forgotPasswordValidation(req.body);
+    if (error)
+        return res.status(400).json({ "message": error.details[0].message });
+
+    // check if email is already in the database
+    const user = await User.findOne({ email });
+    if (!user)
+        return res.status(400).json({ "message": "email is not found" })
+
+    let newPassword = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < 10; i++)
+        newPassword += characters.charAt(Math.floor(Math.random() * charactersLength));
+
+    // hash new password
+    const salt = await bcrypt.genSalt(10);
+    const newHashedPassword = await bcrypt.hash(newPassword, salt);
+    const result = await User.updateOne({ email }, { "password": newHashedPassword });
+
+    if (!result.acknowledged || !result.modifiedCount)
+        return res.status(400).json({ "message": "request new password", "status": false });
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        secure: false,
+        auth: {
+            user: process.env.GMAIL_ACCOUNT,
+            pass: process.env.GMAIL_PASSWORD,
+        },
+    });
+
+    const mailOptions = {
+        from: process.env.GMAIL_ACCOUNT, // sender
+        // to: email, // receiver
+        to: email, // receiver
+        subject: '從設密碼', // subject
+        text: '新密碼為: ' + newPassword, // plain text body
+        // html: "<b>Hello world?</b>", // html body
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ "message": "request new password", "status": true });
+    } catch (error) {
+        res.status(400).json({ "message": "request new password", "status": false });
+    }
+
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express';
 
-import { register, login } from '../controller/Auth';
+import { register, login, newPassword, forgotPassword } from '../controller/Auth';
 
 export const router = Router();
 
-
 router.post('/register', register);
 router.post('/login', login);
+
+router.patch('/newpassword', newPassword);
+router.patch('/forgotpassword', forgotPassword);
