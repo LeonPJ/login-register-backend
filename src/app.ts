b3761b66e262e7b07002a2bee0dbcae861ab6654
LeonPJ <leon19941214@gmail.com>
2022-11-08 22:09:13 +0800

    move server port to .env

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+
 // import router
 import { router as authRoute } from './routes/auth';
 import { router as orderRoute } from './routes/order';
@@ -29,4 +31,4 @@ app.use(express.json());
 app.use('/user/', authRoute);
 app.use('/order/', orderRoute);
 
-app.listen(3000, () => console.log('server running'));
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
